648634f3728851fd1660e5c3a2d0fc9caa07fc4d
Axiinos <syrusbrian@gmail.com>
2024-10-16 11:54:09 +0200

    Added player class, and the object is now drawn at the center of the screen.

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    
+    # Initialize pygame
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Get time object
+    clock = pygame.time.Clock()
+    
+    # Delta time
+    dt = 0
+    
+    # Set up the display
+    screen = pygame.display.set_mode((SCREEN_WIDTH,
+                                      SCREEN_HEIGHT))
+    
+    # Player Object Spawn
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    
+    # Infinite loop, keeps the game running until event has been triggered.
     while True:
-        pygame.Surface.fill(screen, color=(0, 0, 0))
-        pygame.display.flip()
+        
+        
+        # Listen for event, such as QUIT
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Fill the screen with black and draw the player
+        screen.fill("black")
+        player.draw(screen)
+        
+        # Update the display
+        pygame.display.flip()
+        
+        # limit frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
     
 if __name__ == "__main__":
     main()
